0a9dbc97c091c2479c86a5eabb421ae324d2fecb
prowebking <irshusvlog@gmail.com>
2024-08-21 00:40:04 +0530

    second commit backend

--- backend/settings.py
+++ backend/settings.py
@@ -143,6 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"assets")
 ]
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"static_cdn","media_root")
 STATIC_URL = '/static/'
